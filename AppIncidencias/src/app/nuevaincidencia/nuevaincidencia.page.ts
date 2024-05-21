d300845c6eb6d78e142a411c2216a7075d064309
miguelcascrii <miguelcastilla2013@gmail.com>
2024-05-22 00:11:51 +0200

    DateTime

--- AppIncidencias/src/app/nuevaincidencia/nuevaincidencia.page.ts
+++ AppIncidencias/src/app/nuevaincidencia/nuevaincidencia.page.ts
@@ -1,17 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Usuario } from '../usuarios';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../servicios/auth/auth.service';
-import { Auth } from 'firebase/auth';
-import { DataServiceService } from '../data-service.service';
-import { Centro } from '../centros';
-import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
-import { Aula } from '../aulas';
-import { Incidencia } from '../incidencias';
-import { Material } from '../materiales';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Usuario } from '../usuarios';
+import { Material } from '../materiales';
+import { Aula } from '../aulas';
+import { IonInput, IonDatetime } from '@ionic/angular';
 
 @Component({
   selector: 'app-nuevaincidencia',
@@ -19,26 +12,31 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./nuevaincidencia.page.scss'],
 })
 export class NuevaincidenciaPage implements OnInit {
-  CodCentro : any;
-  UsuarioYO ?: Usuario;
-  VentanaTitulo : string = ""
-  btnTamPantalla: any
-  ListaMateriales: Material[] = []
-  ListaAulas: Aula[] = []
+  CodCentro: any;
+  UsuarioYO?: Usuario;
+  VentanaTitulo: string = "";
+  btnTamPantalla: any;
+  ListaMateriales: Material[] = [];
+  ListaAulas: Aula[] = [];
+  ListaUsuarios: Usuario[] = [];
+  selectedEmail !: string;
+  selectedNombre ?: string;
+  selectedDate ?: string;
+  @ViewChild('datetime') datetime !: IonDatetime;
+
 
   message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
-  name : any;
+  name: any;
 
   constructor(
-    private authService : AuthService,
-    private toast : ToastController,
+    private authService: AuthService,
     private afAuth: AngularFireAuth,
   ) { }
 
   ngOnInit() {
     const navigation = window.history.state;
     this.VentanaTitulo = navigation.NameVentana;
-    console.log(this.VentanaTitulo)
+    console.log(this.VentanaTitulo);
 
     this.afAuth.authState.subscribe(user => {
       if (user) {
@@ -46,22 +44,25 @@ export class NuevaincidenciaPage implements OnInit {
         this.authService.obtenerUsuarioPorEmail(user.email).subscribe(usuario => {
           this.UsuarioYO = usuario;
           this.CodCentro = usuario?.centro;
+          this.selectedEmail = usuario?.email || ''; // Inicializar selectedEmail
+          this.selectedNombre = usuario?.nombre || ''; // Inicializar selectedNombre
 
           this.listarMateriales(this.CodCentro);
-          this.listarAulas(this.CodCentro)
+          this.listarAulas(this.CodCentro);
+          this.ListarUsuarios(this.CodCentro);
         });
       } else {
         // Realiza cualquier otra acción que necesites cuando el usuario no esté autenticado
       }
-      this.TamañoPantalla()
+      this.TamañoPantalla();
     });
   }
 
   TamañoPantalla() {
     if (window.innerWidth <= 768) {
-      this.btnTamPantalla = false
+      this.btnTamPantalla = false;
     } else {
-      this.btnTamPantalla = true
+      this.btnTamPantalla = true;
     }
   }
 
@@ -88,7 +89,37 @@ export class NuevaincidenciaPage implements OnInit {
       });
     });
   }
-  
 
-  
+  ListarUsuarios(centro: string) {
+    this.authService.DatoWhere(centro, 'Usuarios', 'centro').subscribe((res) => {
+      this.ListaUsuarios = [];
+      res.forEach((element: any) => {
+        this.ListaUsuarios.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data(),
+        });
+      });
+    });
+  }
+
+  onEmailChange(email: string) {
+    const usuario = this.ListaUsuarios.find(user => user.email === email);
+    if (usuario) {
+      this.selectedNombre = usuario.nombre + " " + usuario.apellidos;
+    }
+  }
+
+  onDateChange(event: CustomEvent) {
+    this.selectedDate = event.detail.value;
+  }
+
+  NuevaIncidencia(email: any, nombre: any, aula: any, fecha: any, descripcion: any) {
+    const datetimeValue = this.datetime ? this.datetime.value : null;
+    console.log(email, nombre, aula, fecha, descripcion);
+  }
+
+
+  cancel() {
+    // Implementa la lógica para cancelar la operación
+  }
 }
